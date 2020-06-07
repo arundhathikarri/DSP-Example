a11242a6e6221dfee27b5f083bc4a7266cc747b5
arundhathikarri <65701969+arundhathikarri@users.noreply.github.com>
2020-06-07 19:49:54 +0530

    Initial commit

---
+++ scripts/hng1.js
@@ -0,0 +1,21 @@
+const output = () => {
+  const info = {
+    fullName: "Favour Igwe",
+    hngId: "HNG-01067",
+    email: "favchiazokam@gmail.com",
+    language: "javascript",
+  };
+  return (
+    "Hello  World, this is " +
+    info.fullName +
+    " with HNGi7 ID " +
+    info.hngId +
+    " using " +
+    info.language +
+    " for stage 2 task email " +
+    info.email +
+    "."
+  );
+};
+
+console.log(output());
